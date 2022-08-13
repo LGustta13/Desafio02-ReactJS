f3ff0e222dff66934cb6860155191725397809ac
Luis Gustavo <luisgustavoolivtp@hotmail.com>
2022-08-13 20:42:32 -0300

    Refatorando e criando o componente SideBar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,44 @@
-export function SideBar() {
+import {useState, useEffect} from 'react';
+import {Button} from "./Button";
+import { api } from '../services/api';
+
+type GenreResponseProps = {
+  id: number
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family'
+  title: string 
+}
+
+type SideBarProps = {
+  handleClickButton: (id:number) => void // Função anõmima, parâmetros id e retorna nada
+  selectedGenreId: number
+}
+
+// desemembra os parâmetros
+export function SideBar(props : SideBarProps ) {
   // Complete aqui
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>('genres').then(response => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => props.handleClickButton(genre.id)}
+            selected={props.selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  )
 }
